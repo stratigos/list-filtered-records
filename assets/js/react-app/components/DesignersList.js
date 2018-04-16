3e9c33a253d625feac7f6f8aa3f1ea763a02be8d
Todd Morningstar <techarchist@gmail.com>
2018-04-16 18:18:38 -0400

    Add style to designers page :bouquet:
     * Fix brunch config compile errors with 'ignore' option
     * Add specific ordering of css/scss files to app.scss
     * Add new scss files for vars and components
     * Update styles
     * Create Header component
     * Add images to Designer component
     * Add flex rules to control display
     * Fix attr "class" with "className" in some Components

--- assets/js/react-app/components/DesignersList.js
+++ assets/js/react-app/components/DesignersList.js
@@ -3,7 +3,7 @@ import Designer from "./Designer";
 
 const DesignersList = ({ designers }) => {
   return(
-    <div className="designers-list-container d-flex justify-content-around">
+    <div className="designers-list-container d-flex flex-wrap justify-content-around">
       {designers.map( (designerRecord) => {
         return(<Designer key={designerRecord.designer.id} designer={designerRecord.designer} />);
       })}
---
+++ assets/js/react-app/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = () => {
+  return(
+    <header className="real-designers-app-header text-center">
+      <h1 className="react-app-title display-5">Real Designers</h1>
+      <p className="text-info">List, filter, and mark your favorite designers.</p>
+    </header>
+  );
+};
+
+export default Header;
