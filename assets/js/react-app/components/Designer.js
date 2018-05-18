e858f9488ee8c80d98e80f807ced80a67b83de12
Todd Morningstar <todd@thoughtbot.com>
2018-05-18 16:26:40 -0700

    Update Designer Component
     * Update "favorite" text to be button with icon
     * Update functional Component to be Class
     * Update Designer Component to have test handler for toggling fav
     * Update Designer Component style to center the things

--- assets/js/react-app/components/Designer.js
+++ assets/js/react-app/components/Designer.js
@@ -1,22 +1,66 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 /**
- * Functional component for composing `Designer` data.
+ * Provide a wrapper to the Redux store's `dispatch` method, such that the
+ * designer's fav value can be passed to an ActionCreator.
+ */
+let createToggleFavHandler = (dispatch) => {
+  let toggleFavHandler = (designer_id, designer_fav) => {
+    // dispatch(updateDesignerFav(designer_id, designer_fav));
+    console.log(`TOGGLING FAVORITE STATUS FOR ${designer_id} TO ${designer_fav}`);
+  };
+
+  return toggleFavHandler;
+};
+
+/**
+ * Classy component for composing `Designer` data.
  *   - A `Designer` is a data model, or "schema" of the backend Phoenix
  *     application.
  */
-const Designer = (props) => {
+class Designer extends React.Component {
 
-  return(
-    <div className="designer-card-container card border-secondary bg-dark mt-4">
-      <img className="card-img-top" src={`https://robohash.org/${props.designer.id}`} alt="designer image" />
-      <div className="card-body">
-        <h5 className="card-title text-info">{props.designer.name}</h5>
-        <p className="card-text">{props.designer.favorite ? "Favorite" : "Not Favorite"}</p>
+  constructor(props) {
+    super(props);
+    this.toggleFav = createToggleFavHandler(this.props.dispatch);
+  };
+
+  render() {
+    return(
+      <div
+        className="designer-card-container card border-secondary bg-dark mt-4 \
+          text-center"
+      >
+        <img
+          className="card-img-top"
+          src={`https://robohash.org/${this.props.designer.id}`}
+          alt="designer image"
+        />
+        <div className="card-body">
+          <h5 className="card-title text-info">{this.props.designer.name}</h5>
+          <p className="card-text">
+            <button
+              type="button"
+              className="btn btn-lg btn-block favorite-button"
+              onClick={
+                () => this.toggleFav(
+                  this.props.designer.id,
+                  !this.props.designer.favorite
+                )
+              }
+            >
+              {this.props.designer.favorite ?
+                <i className="fa fa-star" title="Favorite" /> :
+                <i className="fa fa-star-o" title="Not Favorite" />
+              }
+            </button>
+          </p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  };
 
 };
 
@@ -38,4 +82,5 @@ Designer.propTypes= {
   }).isRequired
 };
 
-export default Designer;
+export default connect()(Designer);
+
